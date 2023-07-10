6048d2d044d2d0e3c14aa26a561870d1ea3c5993
linh <linhlinhkook00@gmail.com>
2023-07-10 20:16:58 +0700

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,10 +45,6 @@ function animateNumber(finalNumber, duration = 5000, startNumber = 0, callback) 
 window.addEventListener('load', (event) => {
   bind1('.loading').style.display = "none"
 
-  // animateNumber(6186056420, 2000, 0, function(number) {
-  //   const formattedNumber = number.toLocaleString()
-  //   document.getElementById('total-staking').innerText = '$' + formattedNumber;
-  // })
 });
 
 
@@ -57,65 +53,32 @@ var swiper = new Swiper(".swiper-l-banner", {
     slidesPerView: 1,
     centeredSlides: true,
     spaceBetween: 0,
-    effect: 'fade',
+    // effect: 'fade',
     // auto
     autoplay: {
-        delay: 3000,
+        delay: 2000,
         disableOnInteraction: false,
     },
     loop: true,
     loopedSlides: 50,
-    speed: 1000,
-    // initialSlide: 1,
-    grabCursor: true,
-    // navigation: {
-    //     nextEl: ".swiper-new-roadmap-next",
-    //     prevEl: ".swiper-new-roadmap-prev",
+    // speed: 1000,
+    // // initialSlide: 1,
+    // grabCursor: true,
+    // // navigation: {
+    // //     nextEl: ".swiper-new-roadmap-next",
+    // //     prevEl: ".swiper-new-roadmap-prev",
+    // // },
+    // breakpoints: {
+    //     // when window width is >= 480px
+    //     120: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 20
+    //     },
+    //     // when window width is >= 640px
+    //    // when window width is >= 640px
+    //     740: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 30
+    //     }
     // },
-    breakpoints: {
-        // when window width is >= 480px
-        120: {
-            slidesPerView: 1,
-            spaceBetween: 20
-        },
-        // when window width is >= 640px
-       // when window width is >= 640px
-        740: {
-            slidesPerView: 1,
-            spaceBetween: 30
-        }
-    },
-});
-
-var swiper = new Swiper(".swiper-ins", {
-    slidesPerView: 5,
-    centeredSlides: true,
-    spaceBetween: 2,
-    // auto
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    loop: true,
-    loopedSlides: 50,
-    speed: 1000,
-    // initialSlide: 1,
-    grabCursor: true,
-    navigation: {
-        nextEl: ".swiper-btn-next",
-        prevEl: ".swiper-btn-pre",
-    },
-    breakpoints: {
-        // when window width is >= 480px
-        120: {
-            slidesPerView: 1,
-            spaceBetween: 4
-        },
-        // when window width is >= 640px
-       // when window width is >= 640px
-        740: {
-            slidesPerView: 3,
-            spaceBetween: 4
-        }
-    },
 });
